35fd2eae5cd88549f73f1fe966c72a147603e8c4
Caio Pires <caiogodoi_92@hotmail.com>
2023-05-11 14:22:03 -0300

    pre jogo e desabilitar uma letra ja clicada

--- src/Components/Jogo.jsx
+++ src/Components/Jogo.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Letras from './Letras'
-import './Jogo.css'
+import palavras from '../palavras'
 
 export default function Jogo() {
+  let contagemErros = 0
+
+  const [inputDisabled, setInputDisabled] = useState(true)
+  const [botoesDisabled, setBotoesDisabled] = useState(true)
+  const [palavra, setPalavra] = useState('')
+
+  function habilitarLetras() {
+    setBotoesDisabled(false)
+    const palavraSorteada = palavras[Math.floor(Math.random() * palavra.length)]
+    setPalavra(palavraSorteada)
+  }
+
+  function handleLetterClick(letra) {
+    // console.log(letra)
+  }
+
   return (
     <div>
       <header>
@@ -12,15 +28,17 @@ export default function Jogo() {
         <div>
           <img src="assets/forca0.png" alt="" />
         </div>
-        <div className="palavra">
-          <button>Escolher Palavra</button>
-          <dir>Palavra______</dir>
+        <div className="container-palavra">
+          <button className="btn-escolher" onClick={habilitarLetras}>
+            Escolher Palavra
+          </button>
+          <div className="palavra">{palavra.split('').map(() => '_ ')}</div>
         </div>
       </div>
-      <Letras />
-      <div>
+      <Letras disabled={botoesDisabled} onLetterClick={handleLetterClick} />
+      <div className="container-input">
         <h2>Ja sei a palavra!</h2>
-        <input type="text" />
+        <input disabled={inputDisabled} type="text" />
         <button>Chutar</button>
       </div>
     </div>
--- src/Components/Letras.jsx
+++ src/Components/Letras.jsx
@@ -1,15 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import alfabeto from '../alfabeto'
-import './Letras.css'
 
-function AlfabetoBotoes() {
+function Letras(props) {
+  const { disabled, onLetterClick } = props
+  const [letrasClicadas, setLetrasClicadas] = useState([])
+
+  function handleClick(letra) {
+    onLetterClick(letra)
+    console.log(letra)
+    setLetrasClicadas([...letrasClicadas, letra])
+  }
+
   return (
-    <div>
+    <div className="btn-container">
       {alfabeto.map(letra => (
-        <button key={letra}>{letra}</button>
+        <button
+          disabled={disabled}
+          onClick={() => handleClick(letra)}
+          className={`btn-letras ${
+            letrasClicadas.includes(letra) ? 'clicked' : ''
+          }`}
+          key={letra}
+        >
+          {letra.toLocaleUpperCase()}
+        </button>
       ))}
     </div>
   )
 }
 
-export default AlfabetoBotoes
+export default Letras
